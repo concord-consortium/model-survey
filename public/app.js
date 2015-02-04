92e1ce02228e5463615db8b5ddef98af396b06a6
Noah Paessel <knowuh@gmail.com>
2015-02-04 15:14:53 -0500

    Use google spreadsheet and tabletop to retreive review data.

--- public/app.js
+++ public/app.js
@@ -138,16 +138,33 @@ var LibraryList = React.createClass({displayName: "LibraryList",
   },
 
   componentDidMount: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
+    var oldLoadData = function() {
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        success: function(data) {
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    };
+
+    var showInfo = function(data,tabletop) {
+      this.setState({data:data});
+    }.bind(this);
+
+    var loadData = function() {
+      var sheetUrl = "https://docs.google.com/spreadsheets/d/1HH1tsLKopqA0l4U0EiOHTo50rpnm9BrSbqKn8bTbqh0/pubhtml?gid=532028845&single=true";
+      Tabletop.init({ 
+        key: sheetUrl,
+        callback: showInfo,
+        simpleSheet: true 
+      });    
+    }.bind(this);
+    loadData();
+    setInterval(loadData,5000);
   },
 
   render: function() {
